5498748fe25b8081f69ff97b701ee6b931e12f63
YouKnowPrem <thepremsingh2111@gmail.com>
2024-09-12 17:36:47 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ const cards = [
     {
         name: 'Drake',
         occupation: 'Rapper and producer',
-        from: '',
+        from: 'United States',
         image: 'https://uploads-ssl.webflow.com/5f2429f172d117fcee10e819/6172987a792f18104ce0ce14_sam-day.jpg',
         about: [
             '“Blurring the line between design & dev,',
@@ -12,9 +12,9 @@ const cards = [
         ]
     },
     {
-        name: 'neha kakkar',
+        name: 'Neha kakkar',
         occupation: 'Singer',
-        from: '',
+        from: 'India',
         image: 'https://uploads-ssl.webflow.com/5f2429f172d117fcee10e819/615b54ee54045a4769f04eba_sofia-papadopoulou.jpg',
         about: [
             '"Prem‘s eye for detail & design as well as ',
@@ -24,25 +24,26 @@ const cards = [
         ]
     },
     {
-        name: 'BRUNO ARIZIO',
-        occupation: 'Creative Director ',
-        from: 'at Studio BA',
+        name: 'Donald Trump',
+        occupation: '45th U.S. President
+',
+        from: 'United States',
         image: 'https://uploads-ssl.webflow.com/5f2429f172d117fcee10e819/615b54ee0df682e04e089676_bruno-arizio.jpg',
         about: [
-            '"High-skilled designer who creates novel ',
+            '"High-skilled person who creates nobel ',
             'experiences with ease and craft. His ',
             'signature is more vivid on each new project ',
-            'he launches and this is only the start"',
+            'he launches and this is only the start!"',
         ]
     },
     {
-        name: 'ENEA ROSSI',
-        occupation: 'Co-Founder ',
-        from: 'at Adoratorio',
+        name: 'Mukesh Ambani',
+        occupation: 'Indian businessman ,CEO',
+        from: 'Reliance Industries',
         image: 'https://uploads-ssl.webflow.com/5f2429f172d117fcee10e819/615b54ee9805af64275d2b44_enea-rossi.jpg',
         about: [
-            '"A very promising Creative Director and ',
-            'Interactive Designer given his natural ',
+            '"A very promising Creative Dev and ',
+            'Interactive Intellectual given his natural ',
             'aesthetic taste and innate instinct for ',
             'functionality"',
         ]
